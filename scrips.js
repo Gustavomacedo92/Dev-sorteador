69a0833d9decd34cc1a8d862a7c25343c6a92fd2
Gustavomacedo92 <gustavo.mf@outlook.com>
2024-06-02 10:00:39 -0300

    projeto sorteador de numeros

---
+++ scrips.js
@@ -0,0 +1,28 @@
+/*
+
+function generateNunber() {
+
+    const min = Math.ceil(document.querySelector(".inputmin").value)
+    const max = Math.floor(document.querySelector(".inputmax").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
+
+    alert(result)
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
